c1bac62d097ef637cf7eba55e3ff7c1af3655b2b
Juan Moreno <3160886+jupmoreno@users.noreply.github.com>
2021-11-29 19:33:10 -0300

    Fix iterm2 backup

--- scripts/iterm2-config.sh
+++ scripts/iterm2-config.sh
@@ -2,6 +2,6 @@
 set -e -x
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/config"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2"
 # Use the custom preferences
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
