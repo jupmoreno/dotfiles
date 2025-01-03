e97bb159450440c63d61eddeaaa6a04ed2962707
Juan Moreno <3160886+jupmoreno@users.noreply.github.com>
2025-01-03 10:59:47 -0500

    Delete scripts/iterm2-config.sh

--- scripts/iterm2-config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e -x
-
-# Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2"
-# Use the custom preferences
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
